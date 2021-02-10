728548c5d724a1d7606bb70c5ab220fff80308f4
Ricardo <rmvnew@gmail.com>
2021-02-10 18:20:27 -0400

    added feature to keep screen on while the app is open

--- app/src/main/java/com/example/rancho/MainActivity.kt
+++ app/src/main/java/com/example/rancho/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.rancho
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PowerManager
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.rancho.databinding.ActivityMainBinding
@@ -10,6 +16,10 @@ import com.orhanobut.hawk.Hawk
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @SuppressLint("InvalidWakeLockTag", "WakelockTimeout")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,6 +27,8 @@ class MainActivity : AppCompatActivity() {
 
         Hawk.init(this).build()
 
+       window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+      // window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         setContentView(binding.root)
     }
